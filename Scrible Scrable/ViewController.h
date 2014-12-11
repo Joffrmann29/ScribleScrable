2130ff0f0f8673880ab8bca40f632bee66b3345d
Nutech Systems <nutechsystems@Nutechs-MacBook-Air-2.local>
2014-12-11 16:26:57 -0500

    Initial Commit

---
+++ Scrible Scrable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Scrible Scrable
+//
+//  Created by Nutech Systems on 12/11/14.
+//  Copyright (c) 2014 Nutech Systems Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Scrible Scrable/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Scrible Scrable
+//
+//  Created by Nutech Systems on 12/11/14.
+//  Copyright (c) 2014 Nutech Systems Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
